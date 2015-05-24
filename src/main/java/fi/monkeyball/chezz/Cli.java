0c0f2a7fd110490c26581b28849a4a5ea0e3d976
Juho Friman <juho.friman@staff.uta.fi>
2015-05-24 19:18:55 +0300

    Created really simple Cli for printing state of the chess board.

---
+++ src/main/java/fi/monkeyball/chezz/Cli.java
@@ -0,0 +1,26 @@
+package fi.monkeyball.chezz;
+
+import fi.monkeyball.chezz.domain.ChessBoard;
+import fi.monkeyball.chezz.domain.ChessBoardFactory;
+
+/**
+ * Created by juho on 5/24/15.
+ */
+public class Cli {
+
+    public static void main(String[] args) {
+        ChessBoard chessBoard = ChessBoardFactory.initialState();
+        System.out.println(" abcdefgh ");
+        int c = 8;
+        for (ChessBoard.Row squares : chessBoard) {
+            System.out.print(c);
+            for (ChessBoard.Square square : squares) {
+                System.out.print(square.isEmpty() ? " " : "*");
+            }
+            System.out.println(c--);
+        }
+        System.out.println(" abcdefgh ");
+
+    }
+
+}
