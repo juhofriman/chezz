0e05e623e4a3d1ab8fa15b048329a5f61b35ed48
Juho Friman <juho.t.friman@gmail.com>
2015-05-26 22:45:56 +0300

    That check was unneccessary.

--- src/main/java/fi/monkeyball/chezz/domain/Rook.java
+++ src/main/java/fi/monkeyball/chezz/domain/Rook.java
@@ -10,31 +10,32 @@ public class Rook extends Piece {
 
     @Override
     protected void registerMovesOfThisPiece(MoveSet moveSet, ChessBoard chessBoard, ChessBoard.Square location) {
-        ChessBoard.ROW originalRow = location.getRow();
+        ChessBoard.ROW row = location.getRow();
         boolean blocks = false;
-        while(originalRow.north() != null && !blocks) {
-            blocks = moveSet.addIfOnBoard(chessBoard.squareAt(originalRow.north(), location.getColumn()));
-            originalRow = originalRow.north();
-        }
-        originalRow = location.getRow();
-        blocks = false;
-        while(originalRow.south() != null && !blocks) {
-            blocks = moveSet.addIfOnBoard(chessBoard.squareAt(originalRow.south(), location.getColumn()));
-            originalRow = originalRow.south();
+        while(!blocks) {
+            blocks = moveSet.addIfOnBoard(chessBoard.squareAt(row.north(), location.getColumn()));
+            row = row.north();
         }
 
-        ChessBoard.COLUMN originalColumn = location.getColumn();
+        row = location.getRow();
         blocks = false;
-        while(originalColumn.east() != null && !blocks) {
-            blocks = moveSet.addIfOnBoard(chessBoard.squareAt(location.getRow(), originalColumn.east()));
-            originalColumn = originalColumn.east();
+        while(!blocks) {
+            blocks = moveSet.addIfOnBoard(chessBoard.squareAt(row.south(), location.getColumn()));
+            row = row.south();
         }
 
-        originalColumn = location.getColumn();
+        ChessBoard.COLUMN column = location.getColumn();
         blocks = false;
-        while(originalColumn.west() != null && !blocks) {
-            blocks = moveSet.addIfOnBoard(chessBoard.squareAt(location.getRow(), originalColumn.west()));
-            originalColumn = originalColumn.west();
+        while(!blocks) {
+            blocks = moveSet.addIfOnBoard(chessBoard.squareAt(location.getRow(), column.east()));
+            column = column.east();
+        }
+
+        column = location.getColumn();
+        blocks = false;
+        while(!blocks) {
+            blocks = moveSet.addIfOnBoard(chessBoard.squareAt(location.getRow(), column.west()));
+            column = column.west();
         }
     }
 }
