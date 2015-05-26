a655bc016aa4e8a31c78cf63638ac51444a7ab0c
Juho Friman <juho.t.friman@gmail.com>
2015-05-26 15:30:19 +0300

    Introduced ansi-coloring to Cli. Whites are red and blacks are green :)

--- src/main/java/fi/monkeyball/chezz/Cli.java
+++ src/main/java/fi/monkeyball/chezz/Cli.java
@@ -11,11 +11,21 @@ import java.util.List;
  */
 public class Cli {
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
     public static void main(String[] args) {
         ChessBoard chessBoard = ChessBoardFactory.gameStart();
 
         chessBoard.placePiece(new God(Piece.Color.BLACK), ChessBoard.ROW._3, ChessBoard.COLUMN.D);
-        MoveSet possibleMoves = chessBoard.moveSet(ChessBoard.ROW._3, ChessBoard.COLUMN.D);
+        MoveSet possibleMoves = chessBoard.moveSet(ChessBoard.ROW._2, ChessBoard.COLUMN.D);
 
         System.out.println(" abcdefgh ");
         List<String> rows = new LinkedList();
@@ -25,10 +35,10 @@ public class Cli {
             for (ChessBoard.Square square : squares) {
                 if(possibleMoves.contains(square)) {
                     row += square.isEmpty() ? "'" :
-                            square.getPiece().getColor().equals(Piece.Color.WHITE) ? "W" : "B";
+                            square.getPiece().getColor().equals(Piece.Color.WHITE) ? red("W") : green("B");
                 } else {
                     row += square.isEmpty() ? " " :
-                            square.getPiece().getColor().equals(Piece.Color.WHITE) ? "w" : "b";
+                            square.getPiece().getColor().equals(Piece.Color.WHITE) ? red("w") : green("b");
                 }
             }
             row += c++;
@@ -46,6 +56,14 @@ public class Cli {
         System.out.println("b contains black piece");
         System.out.println("W contains white piece for capturing");
         System.out.println("B contains black piece for capturing");
+    }
+
+    private static String green(String character) {
+        return ANSI_GREEN + character + ANSI_RESET;
+    }
+
+    private static String red(String character) {
+        return ANSI_RED + character + ANSI_RESET;
     }
 
 }
